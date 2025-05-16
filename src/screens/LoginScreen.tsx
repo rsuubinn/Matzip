64c2c9756d1b858c779b0e9471cfd7a6a0ff91d0
rsuubinn <rsuubinn@gmail.com>
2025-05-16 16:54:55 +0900

    feat: 네비게이션 구성

---
+++ src/screens/LoginScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+interface LoginScreenProps {}
+
+function LoginScreen({}: LoginScreenProps) {
+  return (
+    <View>
+      <Text>로그인 화면</Text>
+    </View>
+  );
+}
+
+export default LoginScreen;
