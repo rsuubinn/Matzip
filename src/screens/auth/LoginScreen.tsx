186591b471d02e2171303fdbdd66f76cd68b9835
rsuubinn <rsuubinn@gmail.com>
2025-05-22 15:55:40 +0900

    feat: useForm 커스텀 훅 구성

--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -1,53 +1,53 @@
-import React, {useState} from 'react';
-import {SafeAreaView, StyleSheet} from 'react-native';
+import React from 'react';
+import {SafeAreaView, StyleSheet, View} from 'react-native';
 import InputField from '../../components/InputField';
+import CustomButton from '../../components/CustomButton';
+import useForm from '../../hooks/useForm';
+import {validateLogin} from '../../utils';
 
 interface LoginScreenProps {}
 
 function LoginScreen({}: LoginScreenProps) {
-  const [values, setValues] = useState({
-    email: '',
-    password: '',
+  const login = useForm({
+    initialValue: {
+      email: '',
+      password: '',
+    },
+    validate: validateLogin,
   });
-  const [touched, setTouched] = useState({
-    email: false,
-    password: false,
-  });
-  console.log(touched);
-  const handleChangeText = (name: string, text: string) => {
-    setValues({...values, [name]: text});
-  };
-  const handleBlur = (name: string) => {
-    setTouched({...touched, [name]: true});
-  };
+
+  const handleSubmit = () => {};
   return (
     <SafeAreaView style={styles.container}>
-      <InputField
-        placeholder="이메일"
-        error={'이메일을 입력하세요'}
-        touched={touched.email}
-        inputMode="email"
-        value={values.email}
-        onChangeText={text => handleChangeText('email', text)}
-        onBlur={() => handleBlur('email')}
-      />
-      <InputField
-        placeholder="비밀번호"
-        error={'비밀번호를 입력하세요'}
-        touched={touched.password}
-        secureTextEntry
-        value={values.password}
-        onChangeText={text => handleChangeText('password', text)}
-        onBlur={() => handleBlur('password')}
-      />
+      <View style={styles.inputContainer}>
+        <InputField
+          placeholder="이메일"
+          error={login.errors.email}
+          touched={login.touched.email}
+          inputMode="email"
+          {...login.getTextInputProps('email')}
+        />
+        <InputField
+          placeholder="비밀번호"
+          error={login.errors.password}
+          touched={login.touched.password}
+          secureTextEntry
+          {...login.getTextInputProps('password')}
+        />
+      </View>
+      <CustomButton label="로그인" variant="filled" onPress={handleSubmit} />
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    margin: 10,
-    gap: 10,
+    flex: 1,
+    margin: 30,
+  },
+  inputContainer: {
+    gap: 20,
+    marginBottom: 30,
   },
 });
 
